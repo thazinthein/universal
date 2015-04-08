41cf808574804a7772b9c53c17a23db86f8dfc5d
thazin <thazinthein.nov@gmail.com>
2015-04-07 17:26:34 -0800

    latest update

--- wp-content/plugins/woocommerce-wootabs/public/views/public.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package   Woocommerce_Wootabs
- * @author    Your Name <email@example.com>
- * @license   GPL-2.0+
- * @link      http://example.com
- * @copyright 2014 Your Name or Company Name
- */
-?>
